1943c809e2c07a67713cf9ee50d2a03f4fd9b30b
RelegatedLeader <frankalfaro105@gmail.com>
2024-06-18 10:29:18 -0700

    added logo

--- src/PK_Menu/Menu.js
+++ src/PK_Menu/Menu.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../PK_Menu/CSS Files/MenuStyles.css';
+import logo from '../PK_Menu/images/pk-logo-no-background.png'; // Adjust the path as necessary
 
 export default function Menu() {
   return (
     <div className='menu-container'>
       <div className='menu-options'>
-        <h1>PK-1</h1>
+        <img className='logo' src={logo} alt='logo' />
+
         <Link to='login'>
           <button className='glow-on-hover'> Login</button>
         </Link>
